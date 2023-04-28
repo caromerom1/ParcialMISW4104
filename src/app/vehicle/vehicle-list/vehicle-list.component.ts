d2a03524beff1f533ac8b0a3ad7ee862044a394a
Camilo Romero <ca.romerom1@uniandes.edu.co>
2023-04-28 02:45:07 -0500

    Include vehicle brand count

--- src/app/vehicle/vehicle-list/vehicle-list.component.ts
+++ src/app/vehicle/vehicle-list/vehicle-list.component.ts
@@ -1,20 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable, share } from 'rxjs';
 import { Vehicle } from '../vehicle';
 import { VehicleService } from '../vehicle.service';
+
+interface VehicleBrandCount {
+  brand: string;
+  count: number;
+}
 
 @Component({
   selector: 'app-vehicle-list',
   templateUrl: './vehicle-list.component.html',
-  styleUrls: ['./vehicle-list.component.css']
+  styleUrls: ['./vehicle-list.component.css'],
 })
 export class VehicleListComponent implements OnInit {
-
   vehicles$!: Observable<Vehicle[]>;
 
-  constructor(private vehicleService: VehicleService) { }
+  vehicleBrandsCount$!: Observable<VehicleBrandCount[]>;
+
+  constructor(private vehicleService: VehicleService) {}
 
   ngOnInit(): void {
-    this.vehicles$ = this.vehicleService.getVehicles();
+    const vehicles$ = this.vehicleService.getVehicles().pipe(share());
+
+    this.vehicles$ = vehicles$;
+
+    this.vehicleBrandsCount$ = vehicles$.pipe(
+      map(vehicles => {
+        const brandsCount = new Map<string, number>();
+        vehicles.forEach(vehicle => {
+          const brand = vehicle.marca;
+          const count = brandsCount.get(brand) || 0;
+          brandsCount.set(brand, count + 1);
+        });
+        return Array.from(brandsCount.entries()).map(([brand, count]) => ({
+          brand,
+          count,
+        }));
+      }),
+    );
   }
 }
